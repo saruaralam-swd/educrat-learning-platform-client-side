019fa084a6a11a9c59cea4ec14d9c73d49d162d4
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-27 00:50:02 +0600

    pdf work add done

--- src/Pages/Courses/CourseDetails/CourseDetails.js
+++ src/Pages/Courses/CourseDetails/CourseDetails.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-import { FaDownload } from 'react-icons/fa'
+import { FaDownload, FaFileDownload, FaSpinner } from 'react-icons/fa'
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import Pdf from '../Pdf/Pdf';
 
 
 const CourseDetails = () => {
   const course = useLoaderData();
-  const { id, name } = course;
+  const { id, name,  } = course;
+  console.log(course);
 
   return (
     <div>
-      <h3>
-        {name}
-        <Link to='/PDF'><FaDownload className='inline-block' /></Link>
+      <h3 className='flex items-center gap-3'>
+        <span>{name}</span>
+        <span>
+          <PDFDownloadLink document={<Pdf id={id} />} filename="FORM">
+            {({ loading }) => (loading ? <FaSpinner /> : <FaDownload />)}
+          </PDFDownloadLink>
+        </span>
 
       </h3>
       <Link to={`/course/${id}`}><button className='bg-violet-500 text-white px-4 py-1 rounded-md'>get premium access</button></Link>
