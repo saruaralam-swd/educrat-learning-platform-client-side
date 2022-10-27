46f8e6209bdecbe806cd78e7af8863612e6d1f98
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-27 16:33:47 +0600

    add pdf file some data

--- src/Pages/Courses/Pdf/Pdf.js
+++ src/Pages/Courses/Pdf/Pdf.js
@@ -39,13 +39,21 @@ const styles = StyleSheet.create({
 });
 
 const Pdf = ({ course }) => {
-  const { cardImg, instructor, name, body, } = course;
+  const { name, cardImg, student, create, lesson, update, language, body, categoryId, deadLine, enroll, instructor, instructorSay, price, startCourse, time, type } = course;
 
   return (
     <Document>
       <Page style={styles.body}>
         <Text style={styles.header} fixed>{name}</Text>
         <Text style={styles.text}>Instructor: {instructor}</Text>
+        <Text style={styles.text}>Created by: {create}</Text>
+        <Text style={styles.text}>Modify: {update}</Text>
+        <Text style={styles.text}>Language: {language}</Text>
+        <Text style={styles.text}>Students: {student}</Text>
+        <Text style={styles.text}>Time: {time}</Text>
+        <Text style={styles.text}>Type: {type}</Text>
+        <Text style={styles.text}>DeadLine: {deadLine}</Text>
+        <Text style={styles.text}>Price: TK{price}</Text>
         <Text style={styles.text}>{body}</Text>
         <Text
           style={styles.pageNumber}
